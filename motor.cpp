e03e76ef3b31c1b2344cf7d2661be311c4305239
Bryson Smith <15smith.bryson@gmail.com>
2023-08-03 20:58:45 -0500

    Fixed motor file after corruption

--- motor.cpp
+++ motor.cpp
@@ -1,7 +1,6 @@
 #include "motor.h"
 #include <math.h>
 #include <softPwm.h>
-#include <tuple>
 #include <wiringPi.h>
 
 Motor::Motor(int in1, int in2, int ENA) {
@@ -13,13 +12,13 @@ Motor::Motor(int in1, int in2, int ENA) {
   this->in1 = in1;
   this->in2 = in2;
   this->direction = true;
-  this->ramper = Ramper{0.001, 0.05};
+  this->ramper = Ramper{0.0001, 0.05};
 }
 
 void Motor::setPower(float power) {
   // ramp power so that the motor does not jump to a value too fast
   // and potentially damage it
-  power = this->ramper.get(power) * 100;
+  power = (int)(this->ramper.get(power) * 100);
   if (power != 0) {
     // clamp power to -100<=p<=100
     if (power > 100) {
@@ -27,15 +26,29 @@ void Motor::setPower(float power) {
     } else if (power < -100) {
       power = -100;
     }
-    this->direction = power > 0;
-    digitalWrite(this->in1, direction); 
-    digitalWrite(this->in2, !direction);
-    softPwmWrite(this->ENA, power);
+		if ((this->direction && (power < 0)) || (!this->direction && (power > 0))) {
+			// direction change
+			this->lastReversed = std::chrono::high_resolution_clock::now();
+			this->direction = power > 0;
+		}
+		std::chrono::duration<double> directionChangedCounter = std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::high_resolution_clock::now() - this->lastReversed);
+		if (directionChangedCounter >= std::chrono::duration<double>{1.0}) {
+			this->direction = power > 0;
+			digitalWrite(this->in1, direction); 
+			digitalWrite(this->in2, !direction);
+			softPwmWrite(this->ENA, std::abs(power));
+		} else {
+			// set float
+			digitalWrite(this->in1, 1);
+			digitalWrite(this->in2, 1);
+			softPwmWrite(this->ENA, 0);
+		}
   } else {
     // set float
     digitalWrite(this->in1, 1);
     digitalWrite(this->in2, 1);
     softPwmWrite(this->ENA, 0);
+		this->lastReversed = std::chrono::high_resolution_clock::now();
   }
 }
 
